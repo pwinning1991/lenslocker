5e671e2e0047a12d318d4402a07b6e96b9dee2fe
Phil <ftcpjw@yahoo.com>
2022-10-01 14:32:06 -0400

    using a custom router type

--- main.go
+++ main.go
@@ -5,7 +5,33 @@ import (
 	"net/http"
 )
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
+//Moved code into type router and used as a mux
+//func pathHandler(w http.ResponseWriter, r *http.Request) {
+//switch r.URL.Path {
+//case "/":
+//homeHandler(w, r)
+//case "/contact":
+//contactHandler(w, r)
+//default:
+//http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+
+//}
+
+//}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1> Welcome to my awesome site!</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, "<h1>Contact Page</h1><p>To get in touch email me at <ahref=\"mailto:test@gmail.com\">test@gmail.com<\a>")
+}
+
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
@@ -18,19 +44,9 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1> Welcome to my awesome site!</h1>")
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprintf(w, "<h1>Contact Page</h1><p>To get in touch email me at <ahref=\"mailto:test@gmail.com\">test@gmail.com<\a>")
-}
-
 func main() {
-	http.HandleFunc("/", pathHandler)
+	var router Router
 	fmt.Println("Starting the server on :30000")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", router)
 
 }
