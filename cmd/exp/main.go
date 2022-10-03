69ccb724983076c5934a55b8089ff72dfea2b918
Phil <ftcpjw@yahoo.com>
2022-10-02 22:42:30 -0400

    testing templates

---
+++ cmd/exp/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+type User struct {
+	Name string
+}
+
+func main() {
+	t, err := template.ParseFiles("hello.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	user := User{
+		Name: "Phil",
+	}
+
+	err = t.Execute(os.Stdout, user)
+	if err != nil {
+		panic(err)
+	}
+
+}
