eff8db42d3708b64ebbc60c4ab304555ae612692
Phil <ftcpjw@yahoo.com>
2022-10-03 17:48:54 -0400

    added views package

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/pwinning1991/lenslocker
 
 go 1.19
 
-require github.com/go-chi/chi/v5 v5.0.7 // indirect
+require github.com/go-chi/chi/v5 v5.0.7
--- main.go
+++ main.go
@@ -2,29 +2,24 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/pwinning1991/lenslocker/views"
 )
 
 func executeTemplate(w http.ResponseWriter, filepath string) {
 	w.Header().Set("Content-Type", "text/html")
-	tpl, err := template.ParseFiles(filepath)
+	t, err := views.Parse(filepath)
 	if err != nil {
 		log.Printf("parsing template: %v", err)
 		http.Error(w, "There was an error parsing the template", http.StatusInternalServerError)
 		return
 	}
-	err = tpl.Execute(w, nil)
-	if err != nil {
-		log.Printf("error rendering the template: %v", err)
-		http.Error(w, "there was an error rendering the template", http.StatusInternalServerError)
-		return
-	}
+	t.Execute(w, nil)
 
 }
 
