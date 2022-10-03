872063e54280dd2419f53e37317c6524416e0a3c
Phil <ftcpjw@yahoo.com>
2022-10-03 08:50:11 -0400

    adding things to user struct

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -7,6 +7,7 @@ import (
 
 type User struct {
 	Name string
+	Age  int
 }
 
 func main() {
@@ -17,6 +18,7 @@ func main() {
 
 	user := User{
 		Name: "Phil",
+		Age:  31,
 	}
 
 	err = t.Execute(os.Stdout, user)
