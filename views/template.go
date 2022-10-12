d8a2b557b692b8fe6e5af423b57cc41e99e80e52
Phil <ftcpjw@yahoo.com>
2022-10-12 16:30:19 -0400

    updated middleware csrf support

--- views/template.go
+++ views/template.go
@@ -16,7 +16,15 @@ func Must(t Template, err error) Template {
 }
 
 func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
-	tpl, err := template.ParseFS(fs, patterns...)
+	tpl := template.New(patterns[0])
+	tpl.Funcs(
+		template.FuncMap{
+			"csfrField": func() template.HTML {
+				return `<input type="hidden" />`
+			},
+		},
+	)
+	tpl, err := tpl.ParseFS(fs, patterns...)
 	if err != nil {
 		return Template{}, fmt.Errorf("parsing template: %w", err)
 	}
@@ -26,17 +34,17 @@ func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
 	}, nil
 }
 
-func Parse(filepath string) (Template, error) {
-	tpl, err := template.ParseFiles(filepath)
-	if err != nil {
-		return Template{}, fmt.Errorf("parsing template: %w", err)
-	}
-
-	return Template{
-		htmlTpl: tpl,
-	}, nil
-
-}
+//func Parse(filepath string) (Template, error) {
+//	tpl, err := template.ParseFiles(filepath)
+//	if err != nil {
+//		return Template{}, fmt.Errorf("parsing template: %w", err)
+//	}
+//
+//	return Template{
+//		htmlTpl: tpl,
+//	}, nil
+//
+//}
 
 type Template struct {
 	htmlTpl *template.Template
