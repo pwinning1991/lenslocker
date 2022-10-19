0bf7fd03d9f15b8780e227056dcdc9d964d2afd4
Phil <ftcpjw@yahoo.com>
2022-10-19 13:30:37 -0400

    updated the user method in session service

--- models/session.go
+++ models/session.go
@@ -71,7 +71,27 @@ func (ss *SessionService) Create(userID int) (*Session, error) {
 
 func (ss *SessionService) User(token string) (*User, error) {
 	// TODO: Implement SessionsService.User
-	return nil, nil
+	// 1. Hash the session tokens
+	tokenHash := ss.hash(token)
+	// 2. query for the session with that hash
+	var user User
+	row := ss.DB.QueryRow(`
+		SELECT user_id FROM sessions 
+		WHERE token_id = '$1';`, tokenHash)
+	err := row.Scan(&user.ID)
+	if err != nil {
+		return nil, fmt.Errorf("user: %w", err)
+	}
+	//3. Using the UserID from the session, we need to query for that user
+	row = ss.DB.QueryRow(`
+		SELECT email, password_hash from users 
+		WHERE id = $1`, user.ID)
+	err = row.Scan(&user.Email, &user.PasswordHash)
+	if err != nil {
+		return nil, fmt.Errorf("user: %w", err)
+	}
+	//4. Return the User
+	return &user, nil
 }
 
 func (ss *SessionService) hash(token string) string {
